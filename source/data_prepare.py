df60ad65442a69e19008bdb0f26c6efa648058f7
kureck <kureck@gmail.com>
2014-07-11 00:16:01 -0300

    created DataPrepare class

--- source/data_prepare.py
+++ source/data_prepare.py
@@ -19,21 +19,17 @@ class DataPrepare:
       :param args: a list of stock names
     """
     self.headers = args
-    self.samples = self.data(file)
+    self.df = self.read_file(file)
+    self.samples = self.data()
 
 
   def read_file(self, file):
     return pd.read_csv(file)
 
-  def prepare_data(self, df):
-    """
-      formado de saída do dataframe deve ser: [(x1, ..., x_n), y]
-      do tipo numpy.array(ROW_SIZE, dtype=[('input',  float, X_N), ('output', float, Y)])
-      a = np.zeros(len(df), dtype=[('input',  float, len(df.columns)), ('output', float, 1.0)])
-      k = [list(x) for x in df.to_records(index=False)]
-      f = np.asarray(k[0])
-      np.copyto(a[0][0],f)
-    """ 
+  def sliced_data(self):
+    return self.df[self.headers]
+  
+  def y_data(self, df):
     df['y'] = np.ones(np.shape(len(df)))
     N = len(df)
     sample = []
@@ -47,9 +43,39 @@ class DataPrepare:
           df['y'][i+1] = y = 0.5
     return df
 
-  def data(self, file):
-    df = self.read_file(file)[self.headers]
-    return self.prepare_data(df)
+  def init_samples(self,df):
+    return np.zeros(len(df), dtype=[('input',  float, len(df.columns)), ('output', float, 1.0)])
+  
+  def prepare_data(self, df):
+    """
+      formado de saída do dataframe deve ser: [(x1, ..., x_n), y]
+      do tipo numpy.array(ROW_SIZE, dtype=[('input',  float, X_N), ('output', float, Y)])
+      a = np.zeros(len(df), dtype=[('input',  float, len(df.columns)), ('output', float, 1.0)])
+      k = [list(x) for x in df.to_records(index=False)]
+      f = np.asarray(k[0])
+      np.copyto(a[0][0],f)
+    """
+    df = self.y_data(df)
+    a = self.init_samples(df)
+    k = [list(x) for x in df.to_records(index=False)]
+    return a
+
+  def data(self):
+    pass
+
+# -----------------------------------------------------------------------------
+if __name__ == '__main__':
+  file = '../data/all_closes_percentage.csv'
+  st1 = 'americas_bvsp'
+  st2 = 'americas_gsptse'
+  st3 = 'americas_ipsa'
+  DP1 = DataPrepare(file, [st1, st2, st3])
+  s_data = DP1.sliced_data()
+  y_data = DP1.y_data(s_data)
+  a = DP1.init_samples(y_data)
+  print a
+
+
 
 
 
